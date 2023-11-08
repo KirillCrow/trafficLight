e1d6ff6377e1edaac9a8d0d8f3ed2d303bd1a871
KirillCrow <vjmim@yandex.ru>
2023-11-08 17:27:12 +0300

    was made the logic for changing colors and activating the button

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var actionButton: UIButton!
+    
+    enum Light {
+        case red
+        case yellow
+        case green
+    }
+    var statusLight: Light = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = redLight.bounds.width * 0.5
+        yellowLight.layer.cornerRadius = yellowLight.bounds.width * 0.5
+        greenLight.layer.cornerRadius = greenLight.bounds.width * 0.5
+        actionButton.layer.cornerRadius = 15
     }
-
-
+    @IBAction func actionButtonDidTapped(_ sender: UIButton) {
+        sender.setTitle("Next", for: .normal)
+        startTrafficLighter()
+    }
+    
+    private func startTrafficLighter() {
+        switch statusLight {
+        case .red:
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            statusLight = .yellow
+        case .yellow:
+            yellowLight.alpha = 1
+            greenLight.alpha = 0.3
+            redLight.alpha = 0.3
+            statusLight = .green
+        case .green:
+            greenLight.alpha = 1
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            statusLight = .red
+        }
+        
+    }
+    
 }
+
+
 
